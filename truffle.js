138c7691f5ea88026e8c494d90590f342e950327
hoang <vinhyenvodoi98@gmail.com>
2019-03-25 23:07:53 +0700

    HDWalletProvider not found

--- truffle.js
+++ truffle.js
@@ -1,4 +1,26 @@
+require('dotenv').config();
+const HDWalletPriver = require('truffle-hdwallet-provider');
+
 module.exports = {
+
+    networks: {
+        ropsten: {
+            provider: function(){
+                return new HDWalletProvider(
+                    process.env.MNEMONIC,
+                    `https://ropsten.infura.io/${process.env.INFURA_API_KEY}`
+                )
+            },
+            gasPrice: 25000000000,
+            network_id: 3
+        },
+        development:{
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        },
+    },
+        
     compilers:{
         solc:{
             optimizer: {
