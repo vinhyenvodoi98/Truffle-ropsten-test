c94d0137d15ac022efe195d7157204a194316329
hoang <vinhyenvodoi98@gmail.com>
2019-03-25 21:52:29 +0700

    them compiler 0.4.24

---
+++ truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+    compilers:{
+        solc:{
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+            version : "0.4.24"
+        }
+    }
+};
