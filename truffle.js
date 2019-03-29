56487ee00ab5c021f915f6189674fceeec9c000d
hoang <vinhyenvodoi98@gmail.com>
2019-03-29 08:20:31 +0700

    add ringkeby network

--- truffle.js
+++ truffle.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const HDWalletProvider = require('truffle-hdwallet-provider');
-var mnemonic = "apple banana coffie apple banana coffie apple banana coffie"
+// var mnemonic = "apple banana coffie apple banana coffie apple banana coffie"
+var mnemonic = "soup click poverty tooth struggle cash heart have supply sport clean quality"
 // var provider = new HDWalletProvider (mnemonic,"https://ropsten.infura.io/v3/c9a0f45995234454849d7a2d3c9124ed")
 
 // TODO HD Wallet
@@ -13,18 +14,27 @@ module.exports = {
 
     networks: {
         ropsten: {
-            provider: function(){
+            provider: ()=>
+                new HDWalletProvider(
+                    mnemonic,
+                    "https://ropsten.infura.io/v3/33067fd895d4482fa44cbe0f5049e96b"
+                )
+            ,
+            gas: 4712388,
+            gasPrice: 100000000000,
+            network_id: 3
+        },
+        rinkeby: {
+            provider: function() {
                 return new HDWalletProvider(
                     mnemonic,
-                    `https://ropsten.infura.io/v3/c9a0f45995234454849d7a2d3c9124ed`
-                )
-            },
-            gasPrice: 25000000000,
-            network_id: 3
+                    "https://rinkeby.infura.io/v3/33067fd895d4482fa44cbe0f5049e96b"
+                    )},
+            network_id: "*"
         },
         development:{
             host: "127.0.0.1",
-            port: 8545,
+            port: 9545,
             network_id: "*"
         },
     },
